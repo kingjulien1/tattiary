e91b8cb878770fbc2b1ab94306740771a9d63171
julian-blaschke <julianblaschke@icloud.com>
2023-04-29 02:20:18 +0200

    chore: setup daisyui

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html data-theme="retro" lang="en">
       <body>{children}</body>
     </html>
   );
