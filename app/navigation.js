51e4f9b87ff55a6bdfb9df7b76174e0381346039
julian-blaschke <julianblaschke@icloud.com>
2023-05-13 16:13:59 +0200

    refactor: links data source structure change

--- app/navigation.js
+++ app/navigation.js
@@ -2,18 +2,26 @@ import Dropdown from "@components/dropdown"
 import Icon from "@components/icon"
 import classNames from "@utils/classnames"
 import Link from "next/link"
-import resources from "./links.json"
+import { navbar } from "./links.json"
+
+const links = navbar.reduce(
+  (grouped, link) => ({
+    ...grouped,
+    [link.group]: [...(grouped[link.group] || []), link],
+  }),
+  {}
+)
 
 export default function Navigation() {
   return (
     <Dropdown className="dropdown-bottom dropdown-start">
       <Dropdown.Button children={<Icon shape="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />} />
       <Dropdown.Menu>
-        {Object.keys(resources).map(topic => (
+        {Object.keys(links).map(topic => (
           <>
             <Dropdown.Menu.Title children={topic} />
-            {resources[topic].map(({ name, location, icon, isProtected }) => (
-              <li key={name} className={classNames({ hidden: isProtected })}>
+            {links[topic].map(({ name, location, icon, isPublic }) => (
+              <li key={name} className={classNames({ hidden: !isPublic })}>
                 <Link href={location} className="rounded-md" children={[<Icon shape={icon} />, name]} />
               </li>
             ))}
--- app/theming.js
+++ app/theming.js
@@ -15,11 +15,7 @@ export default function ThemePicker() {
   return (
     <Dropdown className="dropdown-bottom dropdown-end">
       <Dropdown.Button children={<Icon shape="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z" />} />
-      <Dropdown.Menu className="max-h-64 overflow-y-auto dropdown-content scrollbar-hide">
-        <ul className="grid grid-cols-1 gap-2">
-          <Themes />
-        </ul>
-      </Dropdown.Menu>
+      <Dropdown.Menu className="max-h-64 overflow-y-auto dropdown-content scrollbar-hide" children={<Themes />} />
     </Dropdown>
   )
 }
@@ -31,23 +27,25 @@ function Themes() {
   const { setTheme, theme } = useTheme()
   const [mounted, setMounted] = useState(false)
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+  useEffect(() => setMounted(true), [])
 
-  if (!mounted) return <li className="px-2 py-1 text-center text-xs font-sans font-semibold" children="loading themes..." />
+  if (!mounted) return <div className="p-2 text-center text-xs font-sans font-semibold" children="loading themes..." />
 
-  return themes.map(name => (
-    <li key={name}>
-      <a onClick={() => setTheme(name)} className={classNames({ active: name === theme }, "flex flex-row justify-between rounded-md")}>
-        <span className="text-xs capitalize font-sans font-semibold" children={name} />
-        <div data-theme={name} className="py-1 px-2 flex flex-row gap-1 rounded bg-transparent">
-          <div className="bg-base-100 w-2 h-4 rounded" />
-          <div className="bg-secondary w-2 h-4 rounded" />
-          <div className="bg-accent w-2 h-4 rounded" />
-          <div className="bg-neutral w-2 h-4 rounded" />
-        </div>
-      </a>
-    </li>
-  ))
+  return (
+    <ul className="grid grid-cols-1 gap-2">
+      {themes.map(name => (
+        <li key={name}>
+          <a onClick={() => setTheme(name)} className={classNames({ active: name === theme }, "flex flex-row justify-between rounded-md")}>
+            <span className="text-xs capitalize font-sans font-semibold" children={name} />
+            <div data-theme={name} className="py-1 px-2 flex flex-row gap-1 rounded bg-transparent">
+              <div className="bg-base-100 w-2 h-4 rounded" />
+              <div className="bg-secondary w-2 h-4 rounded" />
+              <div className="bg-accent w-2 h-4 rounded" />
+              <div className="bg-neutral w-2 h-4 rounded" />
+            </div>
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
 }
